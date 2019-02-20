0b5e92d0a3ed38a46f284f5edc5c3b1d452cfefd
Maxime B <maxime.boulmeck@gmail.com>
2019-02-20 02:09:33 +0100

    adding multiple images support

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 #include <fstream>
 
 int main(int argc, char** argv){
-   std::string coord = "result.txt";
+   std::string coord = argv[1];
    std::ifstream fin(coord);
 
     std::string line;
     std::string linebis;
     std::string to;
-
+    for ( int i = 1; i <= argc; i++ ) {
     const char comma = ',';
     int compteur = 0;
     int compteurbis = 0;
@@ -24,29 +24,25 @@ int main(int argc, char** argv){
             compteurbis = 0;
             std::stringstream ss(line);
                     while(std::getline(ss,to,'\n')){
-                        //std::cout <<"end of line is " << to << '\n';
-                        switch(compteurbis) {
+                    switch(compteurbis) {
                     case 0:
                         {
                         yMax = std::stoi(to);
-                        //std::cout << xMin << ' ' << yMin << ' ' << xMax << ' ' << yMax << '\n';
                         compteurbis = 1;
 
                         // Read img
-                        cv::Mat img = cv::imread("1546070602.png");
+                        cv::Mat img = cv::imread(argv[2]);
                         cv::Mat croppedImg;
 
                         // This line picks out the rectangle from the image
                         // and copies to a new Mat
                         img(cv::Rect(xMin,yMin,xMax,yMax)).copyTo(croppedImg);
 
-                        // Display diff
-                        //cv::imshow( "Original Image",  img );
-                        cv::imshow( "Cropped Image",  croppedImg);
-                        name = "image_"+ std::to_string(compteur_image)+".jpeg";
+                        // Display diff (no more necessary)
+                        //cv::imshow( "Cropped Image",  croppedImg);
+                        name = "image"+std::to_string(i)+"_"+ std::to_string(compteur_image)+".jpeg";
                         compteur_image++;
                         cv::imwrite(name, croppedImg);
-                        //cv::waitKey();
 
                         break;
                         }
@@ -60,7 +56,6 @@ int main(int argc, char** argv){
                     }
             }
         else {
-                //std::cout <<"compteur"<< compteur<<'\n';
                 switch(compteur) {
                     case 0:
                         xMin = std::stoi(line);
@@ -71,8 +66,6 @@ int main(int argc, char** argv){
                     case 2:
                         xMax = std::stoi(line);
                         break;
-                    //default:
-                        //std::cout << "line bug" << compteurbis << '\n';
                     }
             }
         compteur++;
@@ -81,5 +74,6 @@ int main(int argc, char** argv){
             compteur++;
         }
 }
+    }
 }
 
